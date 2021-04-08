d3b8e19028b33cd8fa528b267c17ab692ab374b1
Christian Roese <christian.roese@mortonbuildings.com>
2021-04-08 10:05:04 -0500

    finishes ch 2.4, basic parsing of let stmts, tests

---
+++ parser/parser_test.go
@@ -0,0 +1,77 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/croese/monkey/ast"
+	"github.com/croese/monkey/lexer"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	require.NotNil(t, program, "ParseProgram()")
+	require.Equal(t, 3, len(program.Statements), "program.Statements")
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	t.Helper()
+	if ok := assert.Equal(t, "let", s.TokenLiteral()); !ok {
+		return ok
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if ok := assert.Equal(t, name, letStmt.Name.Value); !ok {
+		return ok
+	}
+
+	if ok := assert.Equal(t, name, letStmt.Name.TokenLiteral()); !ok {
+		return ok
+	}
+
+	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	t.Helper()
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
